cbc137d2489be4883cb22d4957b6ab28a19ec635
Yoel Rodrigues <yoel.rodrigues@trust.tu-darmstadt.de>
2025-04-03 16:52:22 +0200

    Update start-demo-services.sh

--- start-demo-services.sh
+++ start-demo-services.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+DIR=/home/diot/attack-demo-backend
+cd $DIR
+
 # Start Apache2
 echo "Starting Apache2 service..."
 sudo service apache2 start || { echo "Apache2 service failed to start"; exit 1; }
@@ -10,7 +13,7 @@ cd ReadMine-Mirai-Demo-Files/ || { echo "Failed to change directory"; exit 1; }
 
 # Start cnc in a new terminal
 echo "Starting cnc in a new terminal..."
-gnome-terminal --title="CNC" -- bash -c "sudo ./cnc; exec bash"
+gnome-terminal --title="CNC" -- bash -c "sudo ./cnc"
 
 echo "Changing dir into attack-demo-backend"
 cd ..
@@ -18,5 +21,5 @@ cd ..
 # Launch Backend server in a new terminal
 echo "Launching Backend server in a new terminal..."
 
-gnome-terminal --title="Backend Server" -- bash -c "uvicorn app:app --host 0.0.0.0 --port 8000 --reload; exec bash"
+gnome-terminal --title="Backend Server" -- bash -c "uvicorn app:app --host 0.0.0.0 --port 8000 --reload"
 
